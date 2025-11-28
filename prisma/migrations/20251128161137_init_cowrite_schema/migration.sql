2002e6eb3f9a686f7e2382e050f31485226beaa7
“Iblal” <iblalahmed12@gmail.com>
2025-11-28 16:25:42 +0000

    added prisma schema + migrations + config

---
+++ prisma/migrations/20251128161137_init_cowrite_schema/migration.sql
@@ -0,0 +1,27 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "password_hash" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "documents" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL DEFAULT 'Untitled Document',
+    "owner_id" INTEGER NOT NULL,
+    "yjs_state_blob" BYTEA NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "documents_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- AddForeignKey
+ALTER TABLE "documents" ADD CONSTRAINT "documents_owner_id_fkey" FOREIGN KEY ("owner_id") REFERENCES "users"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
