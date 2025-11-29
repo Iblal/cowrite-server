0a1d9b505f00273a95801a828d05cd3e7f2c7e56
“Iblal” <iblalahmed12@gmail.com>
2025-11-29 11:05:36 +0000

    added logger util

---
+++ src/utils/logger.ts
@@ -0,0 +1,15 @@
+import pino from "pino";
+import dayjs from "dayjs";
+
+const log = pino({
+  base: {
+    pid: false,
+  },
+  transport: {
+    target: "pino-pretty",
+    options: { colorize: true },
+  },
+  timestamp: () => `,"time":"${dayjs().toISOString()}"`,
+});
+
+export default log;
